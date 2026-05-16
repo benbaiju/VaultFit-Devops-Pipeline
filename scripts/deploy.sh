63b9a058ec2daa07d0d1feb4b3712e14ee50b7e3
Ben Baiju <ben.baiju2001@gmail.com>
2026-05-16 18:51:04 +1000

    Add Secrets Manager env integration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,11 +11,17 @@ cd vaultfit
 
 git pull origin main
 
+aws secretsmanager get-secret-value \
+  --secret-id vaultfit/prod/env \
+  --region ap-southeast-2 \
+  --query SecretString \
+  --output text | jq -r '
+to_entries
+| map("\(.key)=\(.value)")
+| .[]
+' > .env
+
 docker pull benbaiju/vaultfit-backend:latest
 docker pull benbaiju/vaultfit-frontend:latest
-
-if [ ! -f .env ]; then
-  cp .env.docker.example .env
-fi
 
 docker-compose up -d --remove-orphans
